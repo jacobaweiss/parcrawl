c3952ab0739c0f647ab062c8c1b5f2eb1f2b7f25
Jack <jacobaweiss@gmail.com>
2012-07-17 10:51:19 -0400

    made penalty error flash more descript

--- app/controllers/penalties_controller.rb
+++ app/controllers/penalties_controller.rb
@@ -7,7 +7,7 @@ class PenaltiesController < ApplicationController
       flash[:success] = "The shame of this player has been set in stone."
       redirect_to @match
     else
-      flash[:error] = "Something went wrong; please try again."
+      flash[:error] = "Something went wrong when trying to add this penalty; please try again."
       redirect_to @match
     end
   end
